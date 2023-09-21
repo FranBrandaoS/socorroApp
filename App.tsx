fe5eb9a8022090ad5484795ce1416d1b3af8f740
FranBrandaoS <franciele.brandao@hotmail.com>
2023-09-20 21:23:16 -0300

    Instalação da biblioteca react navigation e criando pastas

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import Login from './Login';
+import Login from './pages/Login';
 
 export default function App() {
   return (
